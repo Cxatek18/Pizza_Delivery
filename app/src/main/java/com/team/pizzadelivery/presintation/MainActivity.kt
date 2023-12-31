d8ce189f12639fa45007819b2ceac47c932db54b
Cxatek18 <griga2888@mail.ru>
2023-12-31 15:29:19 +0500

    initial commit

---
+++ app/src/main/java/com/team/pizzadelivery/presintation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.team.pizzadelivery.presintation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.team.pizzadelivery.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
