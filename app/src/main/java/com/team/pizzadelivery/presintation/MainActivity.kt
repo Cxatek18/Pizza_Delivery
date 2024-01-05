72756f9dc5144b87b9edc6226c224a8b8d017905
Cxatek18 <griga2888@mail.ru>
2024-01-05 18:08:27 +0500

    android-5: Implemented a ViewModel for MainActivity

--- app/src/main/java/com/team/pizzadelivery/presintation/MainActivity.kt
+++ app/src/main/java/com/team/pizzadelivery/presintation/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.team.pizzadelivery.presintation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModelProvider
 import com.team.pizzadelivery.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 }
